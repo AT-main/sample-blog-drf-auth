368e2001a903c48c3cdfcb6b55754796472b9310
AT-main <amirtajik@gmail.com>
2021-01-31 12:14:38 -0500

    add API schema and documentation

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     # 3rd-party apps
     'rest_framework',
     'rest_framework.authtoken',
+    'drf_yasg',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
